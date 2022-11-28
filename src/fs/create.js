359962224e8235ffe1f388e07f1c4d283a63976e
yaroslav.sklema <yaroslav.sklema@gmail.com>
2022-11-28 21:32:35 +0200

    Implemented create.js file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
-const create = async () => {
-    // Write your code here 
-};
+import { writeFile } from 'node:fs/promises';
+import { Buffer } from 'node:buffer';
 
-await create();
+const create = async () => {
+    try {
+      const text = new Uint8Array(Buffer.from('I am fresh and young'));
+      await writeFile(`./files/fresh.txt`, text, { flag: "wx" });
+    } catch (err) {
+      if (err.code === 'EEXIST') {
+        throw new Error('FS operation failed');
+      }
+      throw new Error(err);
+    }
+};
+try {
+  await create();
+} catch (err) {
+  console.log(err);
+}
