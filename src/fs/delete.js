6a2b037d4681427d01f70aad6d1b7ab637382499
yaroslav.sklema <yaroslav.sklema@gmail.com>
2022-11-29 21:53:59 +0200

    Implemented delete.js file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import { rm } from "node:fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await rm("./files/fileToRemove.txt");
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+    throw new Error(err);
+  }
 };
 
-await remove();
+try {
+  await remove();
+} catch (err) {
+  console.log(err);
+}
