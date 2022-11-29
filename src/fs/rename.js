c39113ea950c5e7aa2e57ab0e16a179c35441089
yaroslav.sklema <yaroslav.sklema@gmail.com>
2022-11-29 21:49:01 +0200

    Implemented rename.js file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,38 @@
-const rename = async () => {
-    // Write your code here 
+import { access, constants, rename as fsRename } from "node:fs/promises";
+
+const errorMessage = "FS operation failed";
+
+const isFileExisting = async (path) => {
+  try {
+    await access(path, constants.R_OK);
+    return true;
+  } catch {
+    return false;
+  }
 };
 
-await rename();
+const rename = async () => {
+  const folderPath = "./files";
+  const newFileName = "properFilename.md";
+
+  try {
+    if (await isFileExisting(`${folderPath}/${newFileName}`)) {
+      throw new Error(errorMessage);
+    }
+
+    await fsRename(
+      `${folderPath}/wrongFilename.txt`,
+      `${folderPath}/${newFileName}`
+    );
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error(errorMessage);
+    }
+    throw new Error(err);
+  }
+};
+try {
+  await rename();
+} catch (err) {
+  console.log(err);
+}
