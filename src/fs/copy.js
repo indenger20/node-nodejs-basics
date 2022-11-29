f8b90a6dab930b122f666c8d9ee035b43af285cd
yaroslav.sklema <yaroslav.sklema@gmail.com>
2022-11-29 21:10:41 +0200

    Implemented copy.js file

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import { cp } from "node:fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await cp("./files", "./files_copy", {
+      recursive: true,
+      errorOnExist: true,
+      force: false,
+    });
+  } catch (err) {
+    if (err.code === "ERR_FS_CP_EEXIST" || err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+    throw new Error(err);
+  }
 };
 
-copy();
+try {
+  await copy();
+} catch (err) {
+  console.log(err);
+}
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,16 +1,16 @@
-import { writeFile } from 'node:fs/promises';
-import { Buffer } from 'node:buffer';
+import { writeFile } from "node:fs/promises";
+import { Buffer } from "node:buffer";
 
 const create = async () => {
-    try {
-      const text = new Uint8Array(Buffer.from('I am fresh and young'));
-      await writeFile(`./files/fresh.txt`, text, { flag: "wx" });
-    } catch (err) {
-      if (err.code === 'EEXIST') {
-        throw new Error('FS operation failed');
-      }
-      throw new Error(err);
+  try {
+    const text = new Uint8Array(Buffer.from("I am fresh and young"));
+    await writeFile(`./files/fresh.txt`, text, { flag: "wx" });
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      throw new Error("FS operation failed");
     }
+    throw new Error(err);
+  }
 };
 try {
   await create();
