db22398bf469808cdf1a07801ef48c71a7692fd5
yaroslav.sklema <yaroslav.sklema@gmail.com>
2022-11-29 23:01:18 +0200

    Implemented read.js file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
-const read = async () => {
-    // Write your code here 
+import { readFile } from "node:fs/promises";
+const read = async (fileName) => {
+  try {
+    const file = await readFile(fileName, {
+      encoding: "utf-8",
+    });
+    console.log("file", file);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+    throw new Error(err);
+  }
 };
 
-await read();
+try {
+  await read('./files/fileToRead.txt');
+} catch (err) {
+  console.log(err);
+}
