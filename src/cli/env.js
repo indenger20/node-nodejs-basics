2ff2f226f138e01c9b8b8e0e5db5961042019173
yaroslav.sklema <yaroslav.sklema@gmail.com>
2022-11-30 16:17:41 +0200

    Implemented env.js file

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envs = process.env;
+    const avalabilityEnvs = Object.keys(envs)
+        .filter((key) => key.startsWith('RSS_'))
+        .map(key => `${key}=${envs[key]}`)
+        .join(';')
+    console.log(avalabilityEnvs);
 };
 
 parseEnv();
