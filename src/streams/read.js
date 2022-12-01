399ad643d14d078d37e80562d55d1ed6045be275
yaroslav.sklema <yaroslav.sklema@gmail.com>
2022-12-01 15:36:43 +0200

    Implemented read.js file

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
-const read = async () => {
-    // Write your code here 
+import { createReadStream } from "node:fs";
+import { getDirAndFileName } from "../helpers/index.mjs";
+
+const read = async (filePath) => {
+  return new Promise((resolve, reject) => {
+    const stream = createReadStream(filePath, { encoding: "utf-8" });
+    stream.on("data", (data) => {
+      process.stdout.write(data);
+    });
+
+    stream.on("end", () => resolve("Success"));
+    stream.on("error", () => reject(new Error("FS operation failed")));
+  });
 };
 
-await read();
+try {
+  const { __dirname } = getDirAndFileName(import.meta.url);
+  await read(`${__dirname}/files/fileToRead.txt`);
+} catch (err) {
+  console.error(err);
+}
