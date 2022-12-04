b58a1ded4ecbccd19f7fea6382a5c5ebf36b1c4f
yaroslav.sklema <yaroslav.sklema@gmail.com>
2022-12-04 14:14:46 +0200

    Implemented cp.js file

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,19 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from "node:child_process";
+import { getDirAndFileName } from "../helpers/index.mjs";
+
+const spawnChildProcess = async (args = []) => {
+  const { __dirname } = getDirAndFileName(import.meta.url);
+
+  const child = fork(`${__dirname}/files/script.js`, args, {
+    stdio: "pipe",
+  });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+
+  child.on("error", (err) => {
+    console.error("error");
+  });
 };
 
-spawnChildProcess();
+spawnChildProcess(["someArg1", ["someArg2"]]);
